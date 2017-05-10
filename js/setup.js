2ad22e2d5798999821964b9bd56062b485fc497a
JiaWei2016 <wjia@upenn.edu>
2017-05-09 22:13:51 -0400

    Final_Jia Wei
    Crash in Philadelphia

--- js/setup.js
+++ js/setup.js
@@ -1,14 +1,17 @@
-//Final project
-//Select a route and give a score, how many collision points there
-//Shwo information such as crash point, crash type, crash time and causes for each point
+//Final project_Jia Wei
+//With this website, you can make routes and know how many crashes along the routes
+//You can know the location of crash point by year and type
+//You can get information about crash data overview by clicking crash overview button
+//data source: open.philadelphia
 
+//map
+//code source: week12, Jquery API documentatin
 var app = {
   apikey: "b4721195dea6cddb0c7c0af3b5cc20a276a605ce",
   map: L.map('map', { center: [39.949965, -75.164779], zoom: 13 }),
   geojsonClient: new cartodb.SQL({ user: 'jiawei2016', format: 'geojson' }),
   drawnItems: new L.FeatureGroup()
 };
-
 
 L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.{ext}', {
  	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
@@ -26,13 +29,13 @@ var state = {
 }
 
 //data
-
 var Crashdata2012=$.ajax("https://raw.githubusercontent.com/dmcglone/philly-crash-data/master/data/2012/all_crashes_2012.geojson")
 var Crashdata2011=$.ajax("https://raw.githubusercontent.com/dmcglone/philly-crash-data/master/data/2011/all_crashes_2011.geojson")
 var Crashdata2010=$.ajax("https://raw.githubusercontent.com/dmcglone/philly-crash-data/master/data/2010/all_crashes_2010.geojson")
 var Crashdata2009=$.ajax("https://raw.githubusercontent.com/dmcglone/philly-crash-data/master/data/2009/all_crashes_2009.geojson")
 var Crashdata2008=$.ajax("https://raw.githubusercontent.com/dmcglone/philly-crash-data/master/data/2008/all_crashes_2008.geojson")
 
+//Create function to groupdata
 var selection=function(x){app.geojsonClient.execute(x)
   .done(function(data) {
     L.geoJson(data, {
@@ -45,73 +48,79 @@ var selection=function(x){app.geojsonClient.execute(x)
   }).error(function(errors) {
   })
  };
+
  //groupdata
-var year2012=function(){selection("SELECT * FROM all_crashes_2012 LIMIT 100")}
-var year2011=function(){selection("SELECT * FROM all_crashes_2011 LIMIT 100")}
-var year2010=function(){selection("SELECT * FROM all_crashes_2010 LIMIT 100")}
-var year2009=function(){selection("SELECT * FROM all_crashes_2009 LIMIT 100")}
-var year2008=function(){selection("SELECT * FROM all_crashes_2008 LIMIT 100")}
+var year2012=function(){selection("SELECT * FROM all_crashes_2012 LIMIT 200")}
+var year2011=function(){selection("SELECT * FROM all_crashes_2011 LIMIT 200")}
+var year2010=function(){selection("SELECT * FROM all_crashes_2010 LIMIT 200")}
+var year2009=function(){selection("SELECT * FROM all_crashes_2009 LIMIT 200")}
+var year2008=function(){selection("SELECT * FROM all_crashes_2008 LIMIT 200")}
+var car_2012=function(){selection("SELECT * FROM all_crashes_2012 WHERE automobile >=1 and bicycle=0 LIMIT 200")}
+var car_2011=function(){selection("SELECT * FROM all_crashes_2011 WHERE automobile >=1 and bicycle=0  LIMIT 200")}
+var car_2010=function(){selection("SELECT * FROM all_crashes_2010 WHERE automobile >=1 and bicycle=0  LIMIT 200")}
+var car_2009=function(){selection("SELECT * FROM all_crashes_2009 WHERE automobile >=1 and bicycle=0 LIMIT 200")}
+var car_2008=function(){selection("SELECT * FROM all_crashes_2008 WHERE automobile >=1 and bicycle=0 LIMIT 200")}
+var bike_2012=function(){selection("SELECT * FROM all_crashes_2012 WHERE bicycle >=1 and automobile=0 LIMIT 200")}
+var bike_2011=function(){selection("SELECT * FROM all_crashes_2011 WHERE bicycle >=1 and automobile=0 LIMIT 200")}
+var bike_2010=function(){selection("SELECT * FROM all_crashes_2010 WHERE bicycle >=1 and automobile=0 LIMIT 200")}
+var bike_2009=function(){selection("SELECT * FROM all_crashes_2009 WHERE bicycle >=1 and automobile=0 LIMIT 200")}
+var bike_2008=function(){selection("SELECT * FROM all_crashes_2008 WHERE bicycle >=1 and automobile=0 LIMIT 500")}
+
+//Remove markers
 var removemarkers = function(markers) {
     _.each(markers,function(m){
       app.map.removeLayer(m)
     });
 };
-var markers=year2012();
-console.log(markers)
-removemarkers(markers)
 
-var car_2012=function(){selection("SELECT * FROM all_crashes_2012 WHERE automobile >=1 LIMIT 100")}
-var car_2011=function(){selection("SELECT * FROM all_crashes_2011 WHERE automobile >=1 LIMIT 100")}
-var car_2010=function(){selection("SELECT * FROM all_crashes_2010 WHERE automobile >=1 LIMIT 100")}
-var car_2009=function(){selection("SELECT * FROM all_crashes_2009 WHERE automobile >=1 LIMIT 100")}
-var car_2008=function(){selection("SELECT * FROM all_crashes_2008 WHERE automobile >=1 LIMIT 100")}
-var bike_2012=function(){selection("SELECT * FROM all_crashes_2012 WHERE bicycle >=1 and automobile=0 LIMIT 100")}
-var bike_2011=function(){selection("SELECT * FROM all_crashes_2011 WHERE bicycle >=1 and automobile=0 LIMIT 100")}
-var bike_2010=function(){selection("SELECT * FROM all_crashes_2010 WHERE bicycle >=1 and automobile=0 LIMIT 100")}
-var bike_2009=function(){selection("SELECT * FROM all_crashes_2009 WHERE bicycle >=1 and automobile=0 LIMIT 100")}
-var bike_2008=function(){selection("SELECT * FROM all_crashes_2008 WHERE bicycle >=1 and automobile=0 LIMIT 100")}
+var layerMarkers = [];
 
-// $("#year").val();
-// $("#type").val();
-
-//Remove function
-
-// var layerMarkers = [];
-//
-// var remove=function(x){
-//   if(layerMarkers.length){
-//     removeMarkers(layerMarkers)
-//     layerMarkers = [];
-//   };
-//     layerMarkers = x;
-// }
-
-
-//   layerMarkers= year2012()
-//   $("#type").change(function(){
-//   if($("#year").val()==="2012" && $("#type").val()==="All types"){
-//         remove(bike_2009());
-//   }else if($("#year").val()==="2012" && $("#type").val()==="cars"){
-//         remove(car_2012());
-//     }
-//    else if($("#year").val()==="2011" && $("#type").val()==="cars"){
-//         remove(car_2011());
-//   }
-//   else if($("#year").val()==="2010" && $("#type").val()==="cars"){
-//         remove(car_2010());
-//   }
-//   else if($("#year").val()==="2009" && $("#type").val()==="cars"){
-//        remove(car_2009());
-//   }
-//   else if($("#year").val()==="2008" && $("#type").val()==="cars"){
-//        remove(car_2008());
-//   }
-//   else{
-//     // year2010()
-//
-//   }
-//   })
-
+var slide=function(x){
+  if(layerMarkers.length){
+    removeMarkers(layerMarkers)
+    layerMarkers = [];
+  };
+    layerMarkers = x;
+}
+$("#year").val()
+$("#type").val()
+//change slides
+   slide(year2012())
+  $("#type").change(function(){
+  if($("#year").val()==="2012" && $("#type").val()==="All types"){
+        slide(year2012());
+  }else if($("#year").val()==="2011" && $("#type").val()==="All types"){
+        slide(car_2011());
+  }else if($("#year").val()==="2010" && $("#type").val()==="All types"){
+        slide(car_2010());
+  }else if($("#year").val()==="2009" && $("#type").val()==="All types"){
+        slide(car_2009());
+  }else if($("#year").val()==="2008" && $("#type").val()==="All types"){
+        slide(car_2008());
+  }else if($("#year").val()==="2012" && $("#type").val()==="cars"){
+        slide(car_2012());
+  }else if($("#year").val()==="2011" && $("#type").val()==="cars"){
+        slide(car_2011());
+  }else if($("#year").val()==="2010" && $("#type").val()==="cars"){
+        slide(car_2010());
+  }else if($("#year").val()==="2009" && $("#type").val()==="cars"){
+        slide(car_2009());
+  }else if($("#year").val()==="2008" && $("#type").val()==="cars"){
+        slide(car_2008());
+  }else if($("#year").val()==="2012" && $("#type").val()==="Bike"){
+        slide(bike_2012());
+  }else if($("#year").val()==="2011" && $("#type").val()==="Bike"){
+        slide(bike_2011());
+  }else if($("#year").val()==="2010" && $("#type").val()==="Bike"){
+        slide(bike_2010());
+  }else if($("#year").val()==="2009" && $("#type").val()==="Bike"){
+        slide(bike_2009());
+  }else if($("#year").val()==="2008" && $("#type").val()==="Bike"){
+        slide(bike_2008());
+  }else{
+        slide(year2012())
+  }
+})
 
 
 //Leaflet draw setup
@@ -148,13 +157,8 @@ app.map.on('draw:created', function (e) {
   console.log('draw created:', e);
 });
 
-
-
 //routes
-/** ---------------
-Leaflet Draw configuration
----------------- */
-
+//code source: week09
 var myMarker = [];
 var demoShapes= [];
 
@@ -170,13 +174,6 @@ var drawControl = new L.Control.Draw({
 console.log(drawControl)
 app.map.addControl(drawControl);
 
-/** ---------------
-Reset application
-
-Sets all of the state back to default values and removes both markers and the line from map. If you
-write the rest of your application with this in mind, you won't need to make any changes to this
-function. That being said, you are welcome to make changes if it helps.
----------------- */
 
 var resetApplication = function() {
   state.count = 0;
@@ -198,19 +195,19 @@ Leaflet Draw runs every time a marker is added to the map. When this happens
 var checkCount = function(){
   if (state.count > 1){
     app.map.removeControl(drawControl);
-    var routeJson = {
+    var route = {
       "locations":[
         {"lat":state.marker1._latlng.lat,"lon":state.marker1._latlng.lng},
         {"lat":state.marker2._latlng.lat,"lon":state.marker2._latlng.lng}],
       "costing":"auto",
       "directions_options":{"units":"miles"}};
-    var routeStr = "https://matrix.mapzen.com/optimized_route?json=" +
-      JSON.stringify(routeJson) + "&api_key=mapzen-fXK4VkV";
+    var routeString = "https://matrix.mapzen.com/optimized_route?json=" +
+      JSON.stringify(route) + "&api_key=mapzen-fXK4VkV";
 
-      $.ajax(routeStr).done(function(routeData){
-       var routePoints = decode(routeData.trip.legs[0].shape);
-       var lineCoor = _.map(routePoints,function(theP){
-         return (theP.reverse());
+      $.ajax(routeString).done(function(Data){
+       var Decode = decode(Data.trip.legs[0].shape);
+       var lineCoor = _.map(Decode,function(x){
+         return (x.reverse());
        });
 
        var lineGeoj = {
@@ -229,10 +226,10 @@ var checkCount = function(){
 
        state.line = L.geoJSON(lineGeoj);
        state.line.addTo(app.map);
-
-     });//end of ajax
-   }//end of if
+     });
+   }
 };
+
 app.map.on('draw:created', function (e) {
     var type = e.layerType; // The type of shape
     var layer = e.layer; // The Leaflet layer for the shape
@@ -247,8 +244,8 @@ app.map.on('draw:created', function (e) {
    $('#button-reset').show();
   });
 
-
 //draw line charts
+//code source: Chart JS Totorial
 const CHART=document.getElementById("lineChart");
 console.log(CHART)
 let lineChart=new Chart(CHART,{
